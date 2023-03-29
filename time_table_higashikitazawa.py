20e28a6fbe6142e3da2444d054fdbc43406da2e6
mitssato <mitssato@cisco.com>
2023-03-29 16:28:59 +0900

    range

--- time_table_higashikitazawa.py
+++ time_table_higashikitazawa.py
@@ -81,7 +81,7 @@ try:
     print ("今日の最低気温は"+low_today.text)
     print ("明日の天気は"+tenki_tomorrow.text.replace('\n','')) 
     print (len(transit))
-    for i in len(transit):
+    for i in range(len(transit)):
         print (transit[i].contents[0])
     print (transit.contents[0])
     print (transit[0].contents[0])
