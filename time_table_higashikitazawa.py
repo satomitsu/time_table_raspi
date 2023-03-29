7cef015781918f2b9891315967b1f3199df16e19
mitssato <mitssato@cisco.com>
2023-03-29 13:32:09 +0900

    aaa

--- time_table_higashikitazawa.py
+++ time_table_higashikitazawa.py
@@ -78,7 +78,7 @@ try:
     print ("今日の最高気温は"+high_today.text)
     print ("今日の最低気温は"+low_today.text)
     print ("明日の天気は"+tenki_tomorrow.text.replace('\n','')) 
-    print (transit.text.replace('\n',''))
+    print (transit)
    
     draw.text((10, 0), today_date, font = font15, fill = 0)
 #    draw.text((20, 20), "Weather:", font = font15, fill = 0)
