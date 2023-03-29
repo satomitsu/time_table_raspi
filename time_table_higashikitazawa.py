cf43cd2615c3a6762cc89648e2eea2527154e2ce
mitssato <mitssato@cisco.com>
2023-03-29 13:43:37 +0900

    _one

--- time_table_higashikitazawa.py
+++ time_table_higashikitazawa.py
@@ -71,7 +71,7 @@ try:
     #明日の天気
     tenki_tomorrow = soup_weather.select_one('#main > div.forecastCity > table > tr > td + td > div > p.pict')
     #電車時刻表示
-    transit = soup_transit.select('#main > div.mainWrp > div.mdStaLineDia > div.elmLineDia > table.tblDiaDetail > tr.hh_5 > td.hour')
+    transit = soup_transit.select_one('#main > div.mainWrp > div.mdStaLineDia > div.elmLineDia > table.tblDiaDetail > tr.hh_5 > td.hour')
     #天気の表示
     print (today_date.text.replace('\n','')+"の天気")
     print ("今日の天気は"+tenki_today.text.replace('\n',''))
